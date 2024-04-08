e375ffb1e35e6ac3654a19e771f8be60fec54eb5
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-04-08 14:52:35 +0300

    Rose Db Service Layer implementation started

---
+++ RosanicSocial.Core/Interfaces/IRoseDbService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using RosanicSocial.Domain.Models;
+
+namespace RosanicSocial.Application.Interfaces {
+    public interface IRoseDbService {
+        Task<Rose> GetRose(string id);
+        Task DeleteRose(string id);
+        void PutRose(Rose rose);
+        void AddRose(Rose rose);
+    }
+}
