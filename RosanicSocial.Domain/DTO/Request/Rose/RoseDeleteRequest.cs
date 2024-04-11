b7d640db6bf8be62abe1a1e0cee39998a0d37a7a
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-04-11 23:37:47 +0300

    Rose Entity Changes

--- RosanicSocial.Domain/DTO/Request/Rose/RoseDeleteRequest.cs
+++ RosanicSocial.Domain/DTO/Request/Rose/RoseDeleteRequest.cs
@@ -1,9 +1,12 @@
 ﻿using RosanicSocial.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RosanicSocial.Domain.DTO.Request.Rose {
     public class RoseDeleteRequest {
+        [Required]
+        public Guid Id { get; set; } = Guid.Empty;
         public RoseEntity ToEntity() {
             return new RoseEntity() {
 
--- RosanicSocial.Domain/Models/RoseEntity.cs
+++ RosanicSocial.Domain/Models/RoseEntity.cs
@@ -1,16 +1,32 @@
-﻿namespace RosanicSocial.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RosanicSocial.Domain.Models
 {
     public class RoseEntity
     {
+        [Required]
         public Guid Id { get; set; } = Guid.Empty;
+        [Required]
         public Guid AuthorId { get; set; } = Guid.Empty;
+        [Required]
         public Guid NumbersId { get; set; } = Guid.Empty;
-        public Guid StatisticId { get; set; } = Guid.Empty;
-        public string? AuthorUsername { get; set; } = string.Empty;
+        public Guid? StatisticId { get; set; } = null;
+        public int? CategoryId { get; set; } = null;
+        public int? TypeId {  get; set; } = null;
+        
+        [Required]
+        [MaxLength(300)]
         public string? Message { get; set; } = string.Empty;
+        public string? MediaUrl { get; set; } = null;
+        [Required]
         public DateTime? PostDate { get; set; } = DateTime.MinValue;
-        public bool? IsUpdated = false;
-        public bool? IsDeleted = false;
+        public bool? IsUpdated { get; set; } = false;
+        public bool? IsDeleted { get; set; } = false;
+        public bool? IsNSFW { get; set; } = false;
+        public bool? IsCarousel { get; set; } = false;
+        public bool? IsPlainText { get; set; } = false;
+        public bool? IsPhoto { get; set; } = false;
+        public bool? IsVideo { get; set; } = false;
         public DateTime? CreatedAt { get; set; } = DateTime.MinValue;
         public DateTime? UpdatedAt { get; set; } = DateTime.MinValue;
     }
